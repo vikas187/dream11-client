52ee08182b7cf7655cf5710213caa026ffe685a8
VIKABHAN <VIKABHAN@VIKABHAN-7490.in.oracle.com>
2021-07-25 11:45:55 +0530

    dev tools remomve

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -9,7 +9,8 @@ export default () => {
             teams: teamsReducer
         }),
         compose(applyMiddleware(thunk),
-        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     );
     store.subscribe(()=>{
         console.log(store.getState());
